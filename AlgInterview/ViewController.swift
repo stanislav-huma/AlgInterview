f982e16b0e15348414193d16dfe1ccb3950f2da3
ashcenso <ashcenso@gmail.com>
2023-11-15 15:00:18 +0200

    Make task description beaty

--- AlgInterview/ViewController.swift
+++ AlgInterview/ViewController.swift
@@ -5,33 +5,34 @@
 //  Created by Ash Censo on 15.11.2023.
 //
 
-//📌 Task
-//Visualize your favorite sorting algorithm, custom UI, swiftUI and animations are welcome in any combination.
-//
-//
-//📖 Details
-//The app should have a UI that allows the user to input an array of integers and a button that starts visualization which reflects sorting of the user input. Also the app should have two buttons: step back and forward which visualize transition to the next and previous step in the algorithm.
-//
-//
-//💡 Tech requirements
-//App should be an iOS application that runs on a modern device (iPhone 13 /14) and latest OS
-//(iOS 16). Compatibility with older devices/OS is not required.
-//
-//You are free to choose Objective-C or Swift, any patterns and approaches. Implementation is
-//not limited at any level, i.e. to build the UI you can choose anything from OpenGL to SwiftUI.
-//Code should compile on Xcode 14/MacOS 13 without errors. We expect you will provide a
-//link to GitHub repo, more than one commit length history is a plus.
-//
-//You can't use any 3rd party dependencies. You have three hours.
-//
-//
-//✏️️ What is considered important
-//Overall architecture of  the project, potential scalability, ease of changes and adding new features. Code style, errors and border conditions handling. If you didn't manage to finish what you planned – please specify what still needs to be done.
-//
-//We hope you like programming as much as we do and will enjoy the task.
-//
-//Have fun :)
+/*
+📌 Task
+Visualize your favorite sorting algorithm, custom UI, swiftUI and animations are welcome in any combination.
 
+
+📖 Details
+The app should have a UI that allows the user to input an array of integers and a button that starts visualization which reflects sorting of the user input. Also the app should have two buttons: step back and forward which visualize transition to the next and previous step in the algorithm.
+
+
+💡 Tech requirements
+App should be an iOS application that runs on a modern device (iPhone 13 /14) and latest OS
+(iOS 16). Compatibility with older devices/OS is not required.
+
+You are free to choose Objective-C or Swift, any patterns and approaches. Implementation is
+not limited at any level, i.e. to build the UI you can choose anything from OpenGL to SwiftUI.
+Code should compile on Xcode 14/MacOS 13 without errors. We expect you will provide a
+link to GitHub repo, more than one commit length history is a plus.
+
+You can't use any 3rd party dependencies. You have three hours.
+
+
+✏️️ What is considered important
+Overall architecture of  the project, potential scalability, ease of changes and adding new features. Code style, errors and border conditions handling. If you didn't manage to finish what you planned – please specify what still needs to be done.
+
+We hope you like programming as much as we do and will enjoy the task.
+
+Have fun :)
+*/
 
 import UIKit
 
